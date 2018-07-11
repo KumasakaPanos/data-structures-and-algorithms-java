5cd59064e3a7b634eb99dd3c6be5cb193881b167
root <root@Gurren.gachi.club>
2018-07-10 18:10:40 -0700

    done

---
+++ src/day02_shift_array_insert/ArrayInsertShift.java
@@ -0,0 +1,18 @@
+package day02_shift_array_insert;
+
+public class ArrayInsertShift {
+    public static int[] ArrayInsert(int[] input, int insert){
+        int[] output = new int[input.length+1];
+        int midpoint = (int) Math.floor(((double) input.length)/2);
+        int position = 0;
+        for(int i = 0; i < input.length; i++){
+            if(position == midpoint){
+                output[position] = insert;
+                position++;
+            }
+            output[position] = input[i];
+            position++;
+        }
+        return output;
+    }
+}
