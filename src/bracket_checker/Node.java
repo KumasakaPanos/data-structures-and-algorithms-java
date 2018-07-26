6496d22b0313e485ea5cdbb6fd617258d980f093
user <user@Gurren.gachi.club>
2018-07-26 13:55:42 -0700

    done

--- src/bracket_checker/Node.java
+++
@@ -1,9 +0,0 @@
-package bracket_checker;
-
-public class Node {
-    Node next;
-    int Data;
-    Node(int Data){
-        this.Data = Data;
-    }
-}
--- src/bracket_checker/bracketChecker.java
+++ src/bracket_checker/bracketChecker.java
@@ -1,28 +1,47 @@
 package bracket_checker;
 
+import java.util.Stack;
+
 public class bracketChecker {
-    public static boolean bracketChecker(String input){
-        Stack parensStack = new Stack();
-        Stack brackStack = new Stack();
-        Stack curlyStack = new Stack();
-        for(int i = 0;i < input.length();i++){
+    public static boolean bracketChecker(String input) {
+
+        Stack<Character> charStack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
             char tempChar = input.charAt(i);
-            if(tempChar=='{'){curlyStack.push(curlyStack.length);}
-            if(tempChar=='('){parensStack.push(parensStack.length);}
-            if(tempChar=='['){brackStack.push(brackStack.length);}
-            if(tempChar=='}'){if(curlyStack.head==null){
-                return false;
-            }curlyStack.pop();}
-            if(tempChar==')'){if(parensStack.head==null){
-                return false;
-            }parensStack.pop();}
-            if(tempChar==']'){if(brackStack.head==null){
-                return false;
-            }brackStack.pop();}
+            if (tempChar == '{') {
+                charStack.push(tempChar);
+            }
+            if (tempChar == '(') {
+                charStack.push(tempChar);
+            }
+            if (tempChar == '[') {
+                charStack.push(tempChar);
+            }
+            if (tempChar == '}') {
+                if (charStack.peek() == '{') {
+                    charStack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (tempChar == ')') {
+                if (charStack.peek() == '(') {
+                    charStack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (tempChar == ']') {
+                if (charStack.peek() == '[') {
+                    charStack.pop();
+                } else {
+                    return false;
+                }
+            }
         }
-        if(curlyStack.length!=1||parensStack.length!=1||brackStack.length!=1){
-            return false;
+        if (charStack.empty()) {
+            return true;
         }
-        return true;
+        return false;
     }
 }
