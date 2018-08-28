12170ca726bc6ea0b247dac056ec9107676cac4c
user <user@Gurren.gachi.club>
2018-08-28 10:15:44 -0700

    done

--- src/number36_mergesort/MergeSort.java
+++ src/number36_mergesort/MergeSort.java
@@ -7,25 +7,37 @@ public class MergeSort {
         if (input.length > 1) {
             int mid = midFindGivenLength(input.length);
             int[] stor1 = mergeSort(Arrays.copyOfRange(input, 0, mid));
-            int[] stor2 = new int [input.length];
-            System.arraycopy(stor1,0,stor2,0,stor1.length);
-            System.arraycopy(mergeSort(Arrays.copyOfRange(input,mid,input.length-1)),0,stor2,stor1.length,input.length);
+            int[] stor2 = mergeSort(Arrays.copyOfRange(input,mid,input.length));
             int[] returnVal = new int [input.length];
+            int j = 0;
+            int k = 0;
             for(int i = 0; i < returnVal.length; i++){
                 int minVal = Integer.MAX_VALUE;
-                int j = 0;
-                while(j<stor2.length){if(stor2[j]<minVal) {minVal = stor2[j];}
+                if(stor1[j]<stor2[k]){
+                    returnVal[i]=stor1[j];
+                    minVal=stor1[j];
                     j++;
+                    if(j>stor1.length-1){
+                        j--;
+                        stor1[j]=Integer.MAX_VALUE;
+                    }
+                }else if(stor2[k]<stor1[j]){
+                    returnVal[i]=stor2[k];
+                    k++;
+                    if(k>stor2.length-1){
+                        k--;
+                        stor2[k]=Integer.MAX_VALUE;
+                    }
                 }
-                stor2[j]=Integer.MAX_VALUE;
             }
+            return returnVal;
         }
         return input;
     }
 
     private static Integer midFindGivenLength(Integer input) {
         if (input % 2 == 0) {
-            return (input / 2) - 1;
+            return (input / 2);
         }
         return Math.floorDiv(input, 2);
     }
--- src/number36_mergesort/MergeSortTest.java
+++ src/number36_mergesort/MergeSortTest.java
@@ -8,16 +8,18 @@ class MergeSortTest {
     void mergeSort() {
         int[] arr1 = {5,6,4,2,7,3,1};
         int[] arrSort = {1,2,3,4,5,6,7};
-        assertEquals(MergeSort.mergeSort(arr1),arrSort);
+        assertArrayEquals(MergeSort.mergeSort(arr1),arrSort);
     }
+    @org.junit.jupiter.api.Test
     void mergeSort1() {
         int[] arr1 = {5};
         int[] arrSort = {5};
-        assertEquals(MergeSort.mergeSort(arr1),arrSort);
+        assertArrayEquals(MergeSort.mergeSort(arr1),arrSort);
     }
+    @org.junit.jupiter.api.Test
     void mergeSort2() {
         int[] arr1 = new int[4];
         int[] arrSort = {0,0,0,0};
-        assertEquals(MergeSort.mergeSort(arr1),arrSort);
+        assertArrayEquals(MergeSort.mergeSort(arr1),arrSort);
     }
 }
